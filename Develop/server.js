620628035ec419f75c523f64eb5bce8848e5d560
tiannaj23 <tiannaj23@gmail.com>
2023-03-20 18:26:26 -0400

    added switch cases

--- Develop/server.js
+++ Develop/server.js
@@ -27,19 +27,37 @@ function start() {
         {
             type: "list",
             name: "startingQuestions",
-            message: "select and option",
-            choices: ["view all employees", "view all departments", "view all roles", "add a department", "add a role", "add an employee", "update a department", "update a role", "update an employee", "quit"]
+            message: "Select an Option",
+            choices: ["View All Employees", "View All Departments", "View All Roles", "Add a Department", "Add a Role", "Add an Employee", "Update a Department", "Update a Role", "Update an Employee", "Quit"]
         }
     ]).then((userResponse)=>{
         console.log("user selected:  " + userResponse.startingQuestions)
         let userChoice = userResponse.startingQuestions;
         switch (userChoice) {
-            case "view all employees":
+            case "View All Employees":
                 viewAllEmployees();
                 break; 
-                default:
-                    console.log("You Blew It")
-                    break
+            case "View All Departments":
+                viewAllDepartments();
+            case "View All Roles":
+                viewAllRoles();
+            case "Add a Department":
+                addADepartment();
+            case "Add a Role":
+                addARole();
+            case "Add an Employee":
+                addAnEmployee();
+            case "Update a Department":
+                updateADepartment();
+            case "Update a Role":
+                updateARole();
+            case "Update an Employee":
+                updateAnEmployee();
+            case "Quit":
+                quit();
+            default:
+                console.log("You Blew It")
+                break
         }
     })
 }
